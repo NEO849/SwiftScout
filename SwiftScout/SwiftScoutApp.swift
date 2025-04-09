b9a69e2fcbf066e55ee0ff659339652db5d684b8
neo849 <f.michi84.989@gmail.com>
2025-04-10 01:42:40 +0200

    ContentView aus der Hauptapp entfernt

--- SwiftScout/SwiftScoutApp.swift
+++ SwiftScout/SwiftScoutApp.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 @main
 struct SwiftScoutApp: App {
+    @State private var isPanelVisible: Bool = true
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FloatingPanelView(
+                isVisible: $isPanelVisible, 
+                viewModel: {
+                    let vm = FunctionPanelViewModel()
+                    vm.loadFunctions()
+                    return vm
+                }()
+            )
         }
     }
+}
+
+#Preview {
+    let viewModel = FunctionPanelViewModel()
+    viewModel.loadFunctions()
+    let isVisible = Binding<Bool>(
+        get: { true },
+        set: { _ in } // Ignorieren
+    )
+    return FloatingPanelView(isVisible: isVisible, viewModel: viewModel)
+        .frame(width: 400, height: 400)
 }
