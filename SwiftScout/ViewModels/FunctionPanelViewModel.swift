a4f9cd62a95dfa13e6966f284109fb6025f31d9a
neo849 <f.michi84.989@gmail.com>
2025-04-09 22:36:49 +0200

    Im Viewmodel func loadData erstellt und initialisiert, die mittels ChooseProvider entweder Mock oder echte Daten lädt.

--- SwiftScout/ViewModels/FunctionPanelViewModel.swift
+++ SwiftScout/ViewModels/FunctionPanelViewModel.swift
@@ -6,21 +6,20 @@
 //
 
 import Foundation
-import Combine
 
 /// Für das Floating Panel. Verarbeitet die Logik, z. B. Daten laden, Filter anwenden, Navigation durchführen.
 class FunctionPanelViewModel: ObservableObject {
     
     @Published var functions: [FunctionModel] = []
     @Published var searchTerm: String = ""
+    
+    init() {
+        loadData()
+    }
 
-    /// Funktion wird später mit den echten SourceKit-Daten befüllt
-    func loadMockData() {
-        // Statische Daten zum Testen der UI
-        self.functions = [
-            FunctionModel(functionName: "fetchData", fileName: "DataService.swift", filePath: URL(fileURLWithPath: "/Pfad/zu/DataService.swift")),
-            FunctionModel(functionName: "calculateScore", fileName: "ScoreViewModel.swift", filePath: URL(fileURLWithPath: "/Pfad/zu/ScoreViewModel.swift")),
-            FunctionModel(functionName: "loginUser", fileName: "AuthService.swift", filePath: URL(fileURLWithPath: "/Pfad/zu/AuthService.swift"))
-        ]
+    /// Entscheidung extern, lädt Daten vom ausgewählten Provider
+    func loadData() {
+        let provider = ChooseProvider.getProvider()
+        self.functions = provider.loadFunctions()
     }
 }
