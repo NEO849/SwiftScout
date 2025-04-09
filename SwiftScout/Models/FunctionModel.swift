37c0e298d7fbef3551c5aec8d747ab845095cb64
neo849 <f.michi84.989@gmail.com>
2025-04-09 11:09:17 +0200

    FunctionModel erstellt, das eine gefunde func Repräsentiert

---
+++ SwiftScout/Models/FunctionModel.swift
@@ -0,0 +1,16 @@
+//
+//  FunctionModel.swift
+//  SwiftScout
+//
+//  Created by Michael Fleps on 09.04.25.
+//
+
+import Foundation
+
+/// Repräsentiert eine gefundene Funktion im Projekt
+struct FunctionModel: Identifiable {
+    let id = UUID()
+    let functionName: String
+    let fileName: String
+    let filePath: URL
+}
