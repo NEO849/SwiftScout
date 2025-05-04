485b06faeff8c174f38f386dae113884f5ac64a3
neo849 <f.michi84.989@gmail.com>
2025-05-04 20:54:28 +0200

    Initial Commit

---
+++ SwiftScout/SwiftScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScoutApp.swift
+//  SwiftScout
+//
+//  Created by Michael Fleps on 04.05.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
