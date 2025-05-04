18d26926fd6d125240ef997528ad6dba43e9bdb2
neo849 <f.michi84.989@gmail.com>
2025-05-05 00:06:16 +0200

    Neues Target erstellt.  Application is agent (UIElement)  auf "Yes" gesetzt, dadurch: Kein störendes Symbol im Dock, App läuft im Hintergrund.

---
+++ SwiftScoutExtension/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  SwiftScoutExtension
+//
+//  Created by Michael Fleps on 04.05.25.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
