5f314994d59a4b60f144581efba75684d45c7919
neo849 <f.michi84.989@gmail.com>
2025-05-05 01:46:50 +0200

    Reference-Model  erstellt. Stellt eine Funktion im Projekt dar.

---
+++ SwiftScout/Models/Reference.swift
@@ -0,0 +1,16 @@
+//
+//  Reference.swift
+//  SwiftScout
+//
+//  Created by Michael Fleps on 05.05.25.
+//
+
+import Foundation
+
+/// Repräsentiert eine Funktionsreferenz mit Name, Dateipfad und Zeilennummer
+struct Reference: Identifiable {
+    let id = UUID()
+    let name: String
+    let filePath: String
+    let lineNumber: Int
+}
